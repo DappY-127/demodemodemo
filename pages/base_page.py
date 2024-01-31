ab7501d583e8cbb4dda2ad5ff9093fb1d947e178
Paul <116675105+DappY-127@users.noreply.github.com>
2024-01-31 16:03:36 +0200

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -27,12 +27,12 @@ class BasePage(HeaderFooterElements):
         # with allure.step(f"Page {self.PAGE_URL} is opened"):
         #     self.wait.until(EC.url_to_be(self.PAGE_URL))
         
-        self.check_and_close_ad_if_present()
+        # self.check_and_close_ad_if_present()
         try:
             with allure.step(f"Page {self.PAGE_URL} is opened"):
-                self.wait.until_not(EC.url_contains("#google_vignette"))
+                # self.wait.until_not(EC.url_contains("#google_vignette"))
                 self.wait.until(EC.url_to_be(self.PAGE_URL))
-                self.check_and_close_ad_if_present()
+                # self.check_and_close_ad_if_present()
         except Exception as e:
             current_url = self.browser.current_url
             self.make_screenshot("Opened page")
