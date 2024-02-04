3d2d2ac19c2ad391d76d9068503d8efa30e25219
Paul <116675105+DappY-127@users.noreply.github.com>
2024-02-04 18:53:53 +0200

    add absolute download path

--- conftest.py
+++ conftest.py
@@ -6,9 +6,6 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="function", autouse=True)
 def browser(request):
-    # preferences = {"download.default_directory": "/usr/workspace/pages/resources"}
-    download_folder = os.path.join(os.getcwd(), 'pages', 'resources')
-
     options = Options()
     options.add_argument('--headless=new')
     options.add_argument('--disable-gpu')
@@ -20,7 +17,7 @@ def browser(request):
     # options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--window-size=1920,1080')
 
-    preferences = {"download.default_directory": download_folder}
+    preferences = {"download.default_directory": "/usr/workspace/pages/resources"}
     options.add_experimental_option("prefs", preferences)
 
     browser = webdriver.Chrome(options=options)
